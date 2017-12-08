a92d03cecc68536d23741567ba01df7a79cc9ede
Gaël Moliner <gmoliner@ds-restauration.com>
2017-12-08 09:58:54 +0100

    add bin

---
+++ bin.js
@@ -0,0 +1,7 @@
+var server = require('./server')
+var minimist = require('minimist')
+var argv = require('minimist')(process.argv.slice(2));
+
+server({port: argv.port || 3300, path: argv.app || 'app'}, (wss) => {
+  console.log('Websocket tourne sur %s (app: %s)', argv.port, argv.app)
+})
