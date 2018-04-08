d8e8e0c1e3697bfea97f829e2a3b0227f5a5de6f
Joe Johans <lgvichy@gmail.com>
2018-04-07 21:14:29 -0400

    Add server test and browser test. Refactor test to macro to reuse between browser and server

---
+++ tests/server.test.js
@@ -0,0 +1,15 @@
+var test = require('tape')
+
+var macro = require('./util-macro')
+
+var server = require('../server')()
+var browserClient = require('../index')
+
+var serverClient = (app, urls) =>
+    browserClient(app, urls, require('websocket').w3cwebsocket)
+
+var PORT = 9001
+
+server.listen(PORT, () => {
+  macro(test, server, serverClient, PORT)
+})
