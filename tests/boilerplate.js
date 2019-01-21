6fb0ae17988ba879b47cd73471b9dd312c3947c1
G-Ray <g.bonneville@qwant.com>
2019-01-21 15:27:34 +0100

    Update dependencies

--- tests/boilerplate.js
+++ tests/boilerplate.js
@@ -7,11 +7,11 @@ module.exports = function (test, server, client, port) {
     var c = client('app', [`localhost:${port}`])
 
     c.subscribe('hello').on('data', function (message) {
-      t.same(message, {hello: 'world'})
+      t.same(message, { hello: 'world' })
       t.end()
       c.close()
     }).on('open', function () {
-      c.broadcast('hello', {hello: 'world'})
+      c.broadcast('hello', { hello: 'world' })
     })
   })
 
@@ -22,19 +22,19 @@ module.exports = function (test, server, client, port) {
     var c1 = client('app1', [`localhost:${port}`])
 
     c1.subscribe('hello').on('data', function (message) {
-      t.same(message, {hello: 'world'})
+      t.same(message, { hello: 'world' })
       done()
     }).on('open', function () {
-      c1.broadcast('hello', {hello: 'world'})
+      c1.broadcast('hello', { hello: 'world' })
     })
 
     var c2 = client('app2', [`localhost:${port}`])
 
     c2.subscribe('hello').on('data', function (message) {
-      t.same(message, {hello: 'world'})
+      t.same(message, { hello: 'world' })
       done()
     }).on('open', function () {
-      c2.broadcast('hello', {hello: 'world'})
+      c2.broadcast('hello', { hello: 'world' })
     })
 
     function done () {
@@ -51,11 +51,11 @@ module.exports = function (test, server, client, port) {
     var c = client('app', [`localhost:${port}/`])
 
     c.subscribe('hello').on('data', function (message) {
-      t.same(message, {hello: 'world'})
+      t.same(message, { hello: 'world' })
       t.end()
       c.close()
     }).on('open', function () {
-      c.broadcast('hello', {hello: 'world'})
+      c.broadcast('hello', { hello: 'world' })
     })
   })
 
@@ -64,7 +64,7 @@ module.exports = function (test, server, client, port) {
     var msgs = ['stranger', 'friend']
 
     c.subscribe(['hello', 'goodbye']).on('data', function (message) {
-      t.same(message, {msg: msgs.shift()})
+      t.same(message, { msg: msgs.shift() })
       if (msgs.length === 0) {
         c.close(function () {
           t.equal(c.subscribers.length, 0, 'all subscribers closed')
@@ -72,8 +72,8 @@ module.exports = function (test, server, client, port) {
         })
       }
     }).on('open', function () {
-      c.broadcast('hello', {msg: 'stranger'}, function () {
-        c.broadcast('goodbye', {msg: 'friend'})
+      c.broadcast('hello', { msg: 'stranger' }, function () {
+        c.broadcast('goodbye', { msg: 'friend' })
       })
     })
   })
