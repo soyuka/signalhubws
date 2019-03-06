7465b4a7199860a9afc0234b5d2e15c5cb2ed872
Martín Acosta <tinchoz49@gmail.com>
2019-03-06 10:23:11 -0300

    fixed tests

--- tests/serverws.test.js
+++ tests/serverws.test.js
@@ -2,7 +2,7 @@ var test = require('tape')
 var macro = require('./boilerplate')
 var server = require('../server')(require('ws').WebSocketServer)
 var browserClient = require('../index')
-var serverClient = (app, urls) => browserClient(app, urls, require('ws'))
+var serverClient = (app, urls) => browserClient(app, urls)
 
 var PORT = 9001
 server.listen(PORT, () => {
