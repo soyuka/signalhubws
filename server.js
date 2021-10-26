c328a5b74d5d71ea191b4aaa2ebaf15100e01bc0
Shachar Itzhaky <corwin.amber@gmail.com>
2021-10-26 22:15:32 +0300

    [bugfix] Implemented server.close().
    To remain compatible with the previous implementation and also allow
    unit-testing of the server. This functionality is currently missing
    from uWebSockets and is polyfilled.
    For similar reasons, each message received from a client is echoed
    to that client. uWebSockets does not follow this semantics.

--- server.js
+++ server.js
@@ -4,11 +4,11 @@ const debug = require('debug')('signalhubws')
 module.exports = function (WebSocketClass, ssl) {
   ssl = ssl === undefined ? false : ssl
 
-  var app
   var Server = WebSocketClass || (ssl ? uWS.SSLApp : uWS.App)
+  var sockets = { server: null, clients: new Set() }
 
   function listen (port, cb) {
-    app = new Server(ssl || {}).ws('/*', {
+    new Server(ssl || {}).ws('/*', {
       /* Server Options */
       compression: uWS.SHARED_COMPRESSOR,
       maxPayloadLength: 16 * 1024 * 1024,
@@ -21,7 +21,7 @@ module.exports = function (WebSocketClass, ssl) {
 
         // need to set `app` in ws when upgrading
         var app = url.split('?')[0].split('#')[0].split('/')
-        res.upgrade({ app: app[app.length - 1] },
+        res.upgrade({ app: app[app.length - 1], echo: true /* echo all messages */ },
           req.getHeader('sec-websocket-key'),
           req.getHeader('sec-websocket-protocol'),
           req.getHeader('sec-websocket-extensions'),
@@ -30,6 +30,7 @@ module.exports = function (WebSocketClass, ssl) {
 
       open (ws) {
         ws.subscribe(ws.app)
+        sockets.clients.add(ws)
       },
 
       message (ws, message, isBinary) {
@@ -37,18 +38,35 @@ module.exports = function (WebSocketClass, ssl) {
 
         debug('Got message', jsond)
 
+        if (ws.echo) ws.send(message)
         ws.publish(ws.app, message, isBinary)
       },
 
       drain: (ws) => { },
-      close: (ws, code, message) => { }
+      close: (ws, code, message) => {
+        debug('Connection closed (code=%s)', code)
+        sockets.clients.delete(ws)
+      }
     }).any('/*', (res, req) => {
       res.end('Signalhubws server')
-    }).listen(port, (token) => {
-      if (token) cb()
-      else { console.error('Failed to listen to port ' + port) }
+    }).listen(port, (listenSocket) => {
+      if (listenSocket) {
+        sockets.server = listenSocket; cb()
+      } else {
+        console.error('Failed to listen to port ' + port)
+      }
     })
   }
 
-  return { listen, close: (cb) => app.close(cb) }
+  function close (cb) {
+    if (sockets.server) {
+      uWS.us_listen_socket_close(sockets.server)
+      sockets.server = null
+    }
+    sockets.clients.forEach(ws => ws.end())
+    sockets.clients.clear()
+    if (cb) cb()
+  }
+
+  return { listen, close }
 }
